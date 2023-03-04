6d4fbe9af084d347457266f9ea0c9d2826e3e685
makuska <sander0204@gmail.com>
2023-03-04 10:49:18 +0200

    Kodutöö 2

--- 02_klass/Kasutaja_Klass/Kasutaja/src/Kasutaja.java
+++ 02_klass/Kasutaja_Klass/Kasutaja/src/Kasutaja.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Kasutaja {
+import javax.xml.crypto.Data;
+import java.util.Date;
+
+public class Kasutaja {
+    String kasutajanimi;
+    String parool;
+    String email;
+    String eesnimi;
+    String perenimi;
+    boolean aktiivne;
+    Date loomisaeg;
+    Date kustutamisAeg;
+
+    public Kasutaja(String kasutajanimi, String parool, String email, String eesnimi, String perenimi){
+        this.kasutajanimi = kasutajanimi;
+        this.parool = parool;
+        this.email = email;
+        this.eesnimi = eesnimi;
+        this.perenimi = perenimi;
+        this.aktiivne = true;
+        this.loomisaeg = new Date();
+        this.kustutamisAeg = null;
+    }
+
+    public String kustutaKasutaja(){
+        this.aktiivne = false;
+        this.kustutamisAeg = new Date();
+        return "Kasutaja kustutatud."; // kuna on mingisugune tagastus, siis peab selle salvestama muututjasse, kui func välja kutsutakse
+    }
+
+    public void muudaParool(String uusParool){
+        this.parool= parool;
+        System.out.println("Parool muudetud!");;
+    }
 }
